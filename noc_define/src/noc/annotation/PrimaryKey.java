c750e5bfdfe8df862b7e76bb457a35402a3850b9
wangshilian <wangshilian@626f0353-5094-f44b-bd95-4260a488012d>
2010-04-11 07:39:22 +0000

    awire by type
    git-svn-id: svn://localhost/noc/trunk@31 626f0353-5094-f44b-bd95-4260a488012d

--- noc_define/src/noc/annotation/PrimaryKey.java
+++ noc_define/src/noc/annotation/PrimaryKey.java
@@ -9,7 +9,12 @@ import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
 
+import noc.lang.Code;
+import noc.lang.ID;
+import noc.lang.Name;
+
 @Retention(RetentionPolicy.RUNTIME) 
 @Target( { FIELD }) 
-@AutoWireByName("key;id;code;name;ID;Name;NAME;姓名;名称;Code;CODE;") 
+@AutoWireByName("key;id;code;name;ID;Name;NAME;姓名;名称;Code;CODE;")
+@AutoWireByType({Name.class,Code.class,ID.class})
 public @interface PrimaryKey {}
