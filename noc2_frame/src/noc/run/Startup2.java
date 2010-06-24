1529bbc71eafa2d8217613b345e7104cfd423b84
wangshilian <wangshilian@626f0353-5094-f44b-bd95-4260a488012d>
2010-06-24 13:30:03 +0000

    add test code
    git-svn-id: svn://localhost/noc/trunk@60 626f0353-5094-f44b-bd95-4260a488012d

---
+++ noc2_frame/src/noc/run/Startup2.java
@@ -0,0 +1,36 @@
+package noc.run;
+
+import java.io.File;
+
+import noc.frame.Vo;
+import noc.frame.persister.filesystem.CsvFilePersister;
+import noc.frame.store.persistable.PersistableStore;
+
+public class Startup2 {
+	public static void main(String[] args) {
+
+		PersistableStore s = new PersistableStore(null);
+		CsvFilePersister p = new CsvFilePersister(new File("d:\\Person.csv"));
+
+		s.setPersistre(p);
+		s.setup();
+
+//		List<Vo> voList = s.list();
+//		for (Vo v : voList) {
+//			System.out.println(v);
+//		}
+
+		Vo v = s.get("wangshilian9");
+
+		System.out.println(v.get("name")  + "@[" + v + "]" );
+		
+		v.put("nakename", "newvalue");
+		
+		v = s.update(v);
+		
+		System.out.println(v.get("nakename")  + "@[" + v + "]" );
+
+		s.cleanup();
+
+	}
+}
