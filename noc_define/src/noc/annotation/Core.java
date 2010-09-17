184bc6b3ab2d583b09e9025f1e3c2dee49f2bf41
wangshilian <wangshilian@626f0353-5094-f44b-bd95-4260a488012d>
2010-09-17 14:48:03 +0000

    把isKey修改为Importance[PrimaryKey,Core,Important,Normal]
    git-svn-id: svn://localhost/noc/trunk@82 626f0353-5094-f44b-bd95-4260a488012d

---
+++ noc_define/src/noc/annotation/Core.java
@@ -0,0 +1,18 @@
+/**
+ * 
+ */
+package noc.annotation;
+
+import static java.lang.annotation.ElementType.FIELD;
+
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+import noc.lang.Name;
+
+@Retention(RetentionPolicy.RUNTIME) 
+@Target( { FIELD }) 
+@AutoWireByName("key;name;Name;NAME;姓名;名称;")
+@AutoWireByType({Name.class})
+public @interface Core {}
