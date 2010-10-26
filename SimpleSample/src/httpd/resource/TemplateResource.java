401e95c3d363d13837b860041bda6e9fd220c185
wangshilian <wangshilian@626f0353-5094-f44b-bd95-4260a488012d>
2010-10-26 06:25:31 +0000

    资源更新检查方法确定
    git-svn-id: svn://localhost/noc/trunk@190 626f0353-5094-f44b-bd95-4260a488012d

--- SimpleSample/src/httpd/resource/TemplateResource.java
+++ SimpleSample/src/httpd/resource/TemplateResource.java
@@ -2,11 +2,10 @@ package httpd.resource;
 
 import java.io.ByteArrayInputStream;
 import java.io.ByteArrayOutputStream;
+import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
-import java.io.PipedInputStream;
-import java.io.PipedOutputStream;
 import java.io.PrintStream;
 import java.io.Reader;
 import java.util.HashMap;
@@ -29,23 +28,26 @@ import freemarker.template.TemplateException;
 public class TemplateResource implements CachableResource {
     private static final Log log = LogFactory.getLog(TemplateResource.class);
 
-    final Configuration engine;
-    // final Configuration preEngine;
+    final protected Configuration engine;
+    final protected Address address;
+
+    final protected String refer;
+    protected String name;
     protected Template template;
-    String name;
 
-    final String refer;
-    final Address address;
+    final protected Store<String, Type> store;
+    final protected String typeName;
 
-    final Store<String, Type> store;
-    final String typeName;
     protected Type type;
+
+    // For Cache Check file
+    final protected int delay = 6000;
+
+    protected long lastChecked;
+    protected long lastModified;
 
     public TemplateResource(Configuration engine, Store<String, Type> store, Address address, String refer) {
         this.engine = engine;
-        // this.preEngine = new Configuration();
-        // this.preEngine.setTemplateLoader(engine.getTemplateLoader());
-
         this.store = store;
         this.typeName = address.getPath().getSegments()[1];
         this.type = store.readData(typeName);
@@ -55,151 +57,143 @@ public class TemplateResource implements CachableResource {
         this.name = typeName + "-" + refer + ".ftl";
     }
 
-    void updateTemplate() {
+    protected void update() {
+        try {
+
+            long srcLastModified = -1;
+
+            TemplateLoader loader = engine.getTemplateLoader();
+            Object o = loader.findTemplateSource(name);
+            if (o != null) {
+                srcLastModified = loader.getLastModified(o);
+            } else {
+                o = loader.findTemplateSource(refer);
+                if (o != null) {
+                    srcLastModified = loader.getLastModified(o);
+                }
+            }
+
+            if (srcLastModified - lastModified > 1000) {
+                reloadTemplate();
+            }
+        } catch (FileNotFoundException e) {
+            throw new RuntimeException(e);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    synchronized protected void reloadTemplate() {
         try {
             TemplateLoader loader = engine.getTemplateLoader();
             Object o = loader.findTemplateSource(name);
-            long lastModified;
+            long sourceModified;
 
             Reader reader;
 
             if (o == null) {
                 Object o1 = loader.findTemplateSource(refer);
-                lastModified = loader.getLastModified(o1);
+                sourceModified = loader.getLastModified(o1);
 
-                if (lastModified - this.lastModified <= 1000) {
+                if (sourceModified - this.lastModified <= 1000) {
                     return;
                 }
 
                 Template combin = engine.getTemplate(refer);
 
                 ByteArrayOutputStream out = new ByteArrayOutputStream();
-                OutputStreamWriter writer = new OutputStreamWriter(out,"utf-8");
+                OutputStreamWriter writer = new OutputStreamWriter(out, "utf-8");
 
                 Map<String, Object> root = new HashMap<String, Object>();
                 root.put("type", type);
                 combin.process(root, writer);
                 writer.close();
 
-                reader = new InputStreamReader(new ByteArrayInputStream(out.toByteArray()),"utf-8");
+                reader = new InputStreamReader(new ByteArrayInputStream(out.toByteArray()), "utf-8");
 
             } else {
-                lastModified = loader.getLastModified(o);
-                if (lastModified - this.lastModified <= 1000) {
+                sourceModified = loader.getLastModified(o);
+                if (sourceModified - this.lastModified <= 1000) {
                     return;
                 }
-
-                reader = loader.getReader(o, null);
+                reader = loader.getReader(o, "utf-8");
             }
 
             Template tempTemplate = new Template(name, reader, engine);
+            
 
-            this.lastModified = lastModified;
-            template = tempTemplate;
+            this.lastModified = sourceModified;
+            this.template = tempTemplate;
 
         } catch (IOException e) {
             throw new RuntimeException(e);
         } catch (TemplateException e) {
             throw new RuntimeException(e);
         }
-
     }
 
     @Override
     public long lastModified() {
-        try {
-            TemplateLoader loader = engine.getTemplateLoader();
-            Object o = loader.findTemplateSource(name);
-            if (o != null) {
-                return loader.getLastModified(o);
-            }
-
-            o = loader.findTemplateSource(refer);
-            if (o != null) {
-                return loader.getLastModified(o);
-            }
-
-            throw new RuntimeException();
-        } catch (IOException e) {
-            throw new RuntimeException(e);
+        long now = System.currentTimeMillis();
+        if (now - lastChecked >= delay) {
+            update();
         }
+        return this.lastModified;
     }
 
     @Override
     public void handle(Request req, Response resp) {
         try {
-            // Cache
-            long req_LastModified = req.getDate("If-Modified-Since");
-            if (req_LastModified > 0) {
-                long now = System.currentTimeMillis();
-
-                if (now - lastChecked >= delay) {
-                    lastModified = this.lastModified();
-                    lastChecked = now;
-
-                    if (lastModified - req_LastModified <= 1000) {
-                        resp.setCode(304);
-                        resp.close();
-                        log.debug(req.getPath() + " Response 304 no change");
-                        return;
-                    }
-                }
+            long now = System.currentTimeMillis();
+            if (now - lastChecked >= delay) {
+                update();
             }
 
-            updateTemplate();
+            // Cache
+            long clientLastModified = req.getDate("If-Modified-Since");
+            if (clientLastModified > 0) {
+
+                if (lastModified - clientLastModified <= 1000) {
+                    resp.setCode(304);
+                    resp.close();
+                    log.debug(req.getPath() + " Response 304 no change");
+                    return;
+                }
+            }
 
             // normal parse
             resp.set("Cache-Control", "max-age=60000000000");
             resp.set("Content-Language", "zh-CN");
             resp.set("Content-Type", "text/html; charset=UTF-8");
-//            resp.setDate("Date", System.currentTimeMillis());
-//            resp.setDate("Last-Modified", lastModified);
-//            resp.set("ETag", "\"" + lastModified + "\"");
-            
-            PrintStream ps =   resp.getPrintStream();
+            // resp.setDate("Date", System.currentTimeMillis());
+            // resp.setDate("Last-Modified", lastModified);
+            // resp.set("ETag", "\"" + lastModified + "\"");
 
-                  
+            PrintStream ps = resp.getPrintStream();
+
             ps.print("<html>");
             ps.print("<head>");
             ps.print("<link href=\"/js/prettify/prettify.css\" type=\"text/css\" rel=\"stylesheet\" />");
             ps.print("<script type=\"text/javascript\" src=\"/js/prettify/prettify.js\"></script>");
-            ps.print("<title>"+ this.name + "</title>");
+            ps.print("<title>" + this.name + "</title>");
             ps.print("</head>");
-            
+
             ps.print("<body onload=\"prettyPrint()\">");
             ps.print("<pre class=\"prettyprint lang-html\" >");
-            
 
-//            ps.print("&lt;tr onload=\"prettyPrint()\"/&gt; sdfdsafdsaf ");
-            
-//
-//            // max-age
-//            log.debug("Transfer file [" + req.getPath() + "] contents to client by [[" + req.getPath() + "]]");
-//            template.dump(resp.getPrintStream());
-//            
-            
-            String  ts = template.toString().replaceAll("<", "&lt;").replaceAll(">", "&gt;");
+            String ts = template.toString().replaceAll("<", "&lt;").replaceAll(">", "&gt;");
 
             ps.print(ts);
-            
+
             ps.print("</pre>");
-            ps.print("</body>");            
+            ps.print("</body>");
             ps.print("</html>");
-            
-            
-            
+
             resp.close();
             return;
         } catch (Exception e) {
             throw new RuntimeException(e);
         }
-
     }
-
-    // For Cache Check file
-    final int delay = 6000;
-    long lastChecked;
-
-    long lastModified;
 
 }
