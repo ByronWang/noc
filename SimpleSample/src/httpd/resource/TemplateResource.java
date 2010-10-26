a0e17f445694edba3a4bdd51f1c6f7c5cecfc0e9
wangshilian <wangshilian@626f0353-5094-f44b-bd95-4260a488012d>
2010-10-26 06:43:22 +0000

    小修正  去除去用的 import
    git-svn-id: svn://localhost/noc/trunk@192 626f0353-5094-f44b-bd95-4260a488012d

--- SimpleSample/src/httpd/resource/TemplateResource.java
+++ SimpleSample/src/httpd/resource/TemplateResource.java
@@ -6,7 +6,6 @@ import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
-import java.io.PrintStream;
 import java.io.Reader;
 import java.util.HashMap;
 import java.util.Map;
