a40fd2c8c36296a7ddf15dbfb29384938f9c434d
wangshilian <wangshilian@626f0353-5094-f44b-bd95-4260a488012d>
2010-10-28 11:31:42 +0000

    git-svn-id: svn://localhost/noc/trunk@256 626f0353-5094-f44b-bd95-4260a488012d

--- noc-frame/src/main/java/noc/lang/reflect/TypeReader.java
+++ noc-frame/src/main/java/noc/lang/reflect/TypeReader.java
@@ -28,276 +28,291 @@ import noc.annotation.Sequence;
 import noc.frame.Store;
 
 public class TypeReader {
-	
-	private static final Log log =LogFactory.getLog(TypeReader.class);
-	
-	final Store<String,Type> types;
-	final CtClass clzScala;
 
-	TypeReader(Store<String,Type> types, CtClass scala) {
-		this.types = types;
-		this.clzScala = scala;
-	}
+    private static final Log log = LogFactory.getLog(TypeReader.class);
 
-	Type fillFrom(CtClass clz, Type type) throws ClassNotFoundException, NotFoundException {
-		Object an = null;
-		type.displayName = (an = clz.getAnnotation(DisplayName.class)) != null ? ((DisplayName) an).value() : clz
-				.getName();
-		
-		log.debug(type.name);
+    final Store<String, Type> types;
+    final CtClass clzScala;
 
-		// // Handle Frame Type
-		// boolean frameType = clz.getAnnotation(FrameType.class) != null;
+    TypeReader(Store<String, Type> types, CtClass scala) {
+        this.types = types;
+        this.clzScala = scala;
+    }
 
-		type.master = Type.Master;
+    Type fillFrom(CtClass clz, Type type) throws ClassNotFoundException, NotFoundException {
+        Object an = null;
+        an = clz.getAnnotation(DisplayName.class);
+        log.debug(type.name + " : " + an);
+        if (an != null) {
+            type.displayName = ((DisplayName) an).value();
+        } else {
+            type.displayName = type.getName();
+        }
 
-		if (clz.getFields().length < 1) {
-			if (clz.isPrimitive() || clz.subtypeOf(clzScala)) {
-				type.master = Type.Scala;
-			} else {
-				type.master = Type.Underlying;
-			}
-			type.standalone = false;
-			return type;
-		}
+        // type.displayName = (an = clz.getAnnotation(DisplayName.class)) !=
+        // null ? ((DisplayName) an).value() : clz
+        // .getName();
 
-		if (clz.getDeclaringClass() != null) {
-			type.master = Type.Eembedded;
-			type.declaringType = types.readData(clz.getDeclaringClass().getName());
-			type.standalone = false;
-		} else {
-			type.standalone = true;
-		}
+        // // Handle Frame Type
+        // boolean frameType = clz.getAnnotation(FrameType.class) != null;
 
-		if (clz.getAnnotation(Master.class) != null) {
-			type.master = Type.Master;
-		} else if (clz.getAnnotation(Sequence.class) != null) {
-			type.master = Type.Sequence;
-		} else if (clz.getAnnotation(Attribute.class) != null) {
-			type.master = Type.Attribute;
-		}
+        type.master = Type.Master;
 
-		if (clz.getAnnotation(Dependent.class) != null) {
-			type.standalone = false;
-			type.master = Type.Eembedded;
-		}
+        if (clz.getFields().length < 1) {
+            if (clz.isPrimitive() || clz.subtypeOf(clzScala)) {
+                type.master = Type.Scala;
+            } else {
+                type.master = Type.Underlying;
+            }
+            type.standalone = false;
+            return type;
+        }
 
-		// Construct type
-		CtField[] cfs = clz.getFields();
+        if (clz.getDeclaringClass() != null) {
+            type.master = Type.Eembedded;
+            type.declaringType = types.readData(clz.getDeclaringClass().getName());
+            type.standalone = false;
+        } else {
+            type.standalone = true;
+        }
 
-		// type = types.get(name);
+        if (clz.getAnnotation(Master.class) != null) {
+            type.master = Type.Master;
+        } else if (clz.getAnnotation(Sequence.class) != null) {
+            type.master = Type.Sequence;
+        } else if (clz.getAnnotation(Attribute.class) != null) {
+            type.master = Type.Attribute;
+        }
 
-		List<Field> fs = type.fields;
-		for (int i = 0; i < cfs.length; i++) {
-			if (!Modifier.isStatic(cfs[i].getModifiers())) {
-				fs.add(readTo(type, cfs[i]));
-			}
-		}
+        if (clz.getAnnotation(Dependent.class) != null) {
+            type.standalone = false;
+            type.master = Type.Eembedded;
+        }
 
-		int countKey = 0;
-		for (Field f : fs) {
-			if (f.importance == Field.PrimaryKey) countKey++;
-		}
+        // Construct type
+        CtField[] cfs = clz.getFields();
 
-		// 如果没有Key的话,按字段名称寻找PrimaryKey中定义的可以默认作为Key的字段
-		if (countKey <= 0 && (an = PrimaryKey.class.getAnnotation(AutoWireByName.class)) != null) {
-			String autoWire = ((AutoWireByName) an).value();
-			for (Field f : fs) {
-				if (f.name.indexOf(autoWire) > 0) {
-					f.importance = Field.PrimaryKey;
-					countKey++;
-					continue;
-				}
-			}
-		}
+        // type = types.get(name);
 
-		// 如果没有Key的话,并且是独立实体的话,设定第一个字段为Key
-		if (fs.size() > 0 && type.standalone) {
-			if (countKey <= 0) {
-				fs.get(0).importance = Field.PrimaryKey;
-			}
-		}
+        List<Field> fs = type.fields;
+        for (int i = 0; i < cfs.length; i++) {
+            if (!Modifier.isStatic(cfs[i].getModifiers())) {
+                fs.add(readTo(type, cfs[i]));
+            }
+        }
 
-		return type;
-	}
+        int countKey = 0;
+        for (Field f : fs) {
+            if (f.importance == Field.PrimaryKey)
+                countKey++;
+        }
 
-	protected Field readTo(Type resideType, CtField ctField) throws ClassNotFoundException, NotFoundException {
-		Object an = null;
+        // 如果没有Key的话,按字段名称寻找PrimaryKey中定义的可以默认作为Key的字段
+        if (countKey <= 0 && (an = PrimaryKey.class.getAnnotation(AutoWireByName.class)) != null) {
+            String autoWire = ((AutoWireByName) an).value();
+            for (Field f : fs) {
+                if (f.name.indexOf(autoWire) > 0) {
+                    f.importance = Field.PrimaryKey;
+                    countKey++;
+                    continue;
+                }
+            }
+        }
 
-		// if (ctField.getName().equals("this$0")) {
-		// }
+        // 如果没有Key的话,并且是独立实体的话,设定第一个字段为Key
+        if (fs.size() > 0 && type.standalone) {
+            if (countKey <= 0) {
+                fs.get(0).importance = Field.PrimaryKey;
+            }
+        }
 
-		String name = ctField.getName();
+        return type;
+    }
 
-		String displayName = (an = ctField.getAnnotation(DisplayName.class)) != null ? ((DisplayName) an).value()
-				: name;
+    protected Field readTo(Type resideType, CtField ctField) throws ClassNotFoundException, NotFoundException {
+        Object an = null;
 
-		/* Handle type */
-		CtClass fieldTypeClazz = ctField.getType();
+        // if (ctField.getName().equals("this$0")) {
+        // }
 
-		Type fieldType = null;
-		boolean array = false;
+        String name = ctField.getName();
 
-		/* construct field */
-		if (fieldTypeClazz.isArray()) {
-			array = true;
-			fieldType = types.readData(fieldTypeClazz.getComponentType().getName());
-		} else if (fieldTypeClazz.getName().equals(java.util.List.class.getName())
-				|| fieldTypeClazz.getName().equals(noc.lang.List.class.getName())) {
-			// Generic field
-			array = true;
-			fieldType = types.readData(decorateActualTypeArguments(ctField).get(0));
-		} else {
-			fieldType = types.readData(fieldTypeClazz.getName());
-		}
+        String displayName = (an = ctField.getAnnotation(DisplayName.class)) != null ? ((DisplayName) an).value()
+                : name;
 
-		if (ctField.hasAnnotation(RealType.class)) {
-			fieldType = types.readData(((RealType) ctField.getAnnotation(RealType.class)).value().getName());
-		}
-		assert fieldType != null;
+        /* Handle type */
+        CtClass fieldTypeClazz = ctField.getType();
 
-		Field field = new Field(name, fieldType);
-		field.array = array;
-		field.displayName = displayName;
+        Type fieldType = null;
+        boolean array = false;
 
-		if (fieldType == resideType) {
-			field.refer = Field.Cascade;
-		} else if (fieldType.master == Type.Scala) {
-			field.refer = Field.Scala;
-		} else if (check(ctField, fieldType.name, Inline.class)) {
-			field.refer = Field.Inline;
-		} else {
-			field.refer = Field.Reference;
-		}
-		if (ctField.hasAnnotation(Reference.class)) field.refer = Field.Reference;
+        /* construct field */
+        if (fieldTypeClazz.isArray()) {
+            array = true;
+            fieldType = types.readData(fieldTypeClazz.getComponentType().getName());
+        } else if (fieldTypeClazz.getName().equals(java.util.List.class.getName())
+                || fieldTypeClazz.getName().equals(noc.lang.List.class.getName())) {
+            // Generic field
+            array = true;
+            fieldType = types.readData(decorateActualTypeArguments(ctField).get(0));
+        } else {
+            fieldType = types.readData(fieldTypeClazz.getName());
+        }
 
-		if (check(ctField, fieldTypeClazz,Important.class)) field.importance = Field.Important;
-		if (check(ctField, fieldTypeClazz,Core.class)) field.importance = Field.Core;
-		if (ctField.hasAnnotation(PrimaryKey.class)) field.importance = Field.PrimaryKey;
+        if (ctField.hasAnnotation(RealType.class)) {
+            fieldType = types.readData(((RealType) ctField.getAnnotation(RealType.class)).value().getName());
+        }
+        assert fieldType != null;
 
-		return field;
-	}
+        Field field = new Field(name, fieldType);
+        field.array = array;
+        field.displayName = displayName;
 
-	protected boolean check(CtField ctField, String typeName, Class<? extends Annotation> anClz)
-			throws ClassNotFoundException, NotFoundException {
-		boolean succeed = false;
-		Annotation an = null;
+        if (fieldType == resideType) {
+            field.refer = Field.Cascade;
+        } else if (fieldType.master == Type.Scala) {
+            field.refer = Field.Scala;
+        } else if (check(ctField, fieldType.name, Inline.class)) {
+            field.refer = Field.Inline;
+        } else {
+            field.refer = Field.Reference;
+        }
+        if (ctField.hasAnnotation(Reference.class))
+            field.refer = Field.Reference;
 
-		an = anClz.getAnnotation(AutoWireByName.class);
-		if (an != null) {
-			AutoWireByName au = (AutoWireByName) an;
-			if (au.value().indexOf(ctField.getName() + ";") >= 0) {
-				succeed = true;
-			}
-		}
+        if (check(ctField, fieldTypeClazz, Important.class))
+            field.importance = Field.Important;
+        if (check(ctField, fieldTypeClazz, Core.class))
+            field.importance = Field.Core;
+        if (ctField.hasAnnotation(PrimaryKey.class))
+            field.importance = Field.PrimaryKey;
 
-		an = anClz.getAnnotation(AutoWireByType.class);
-		if (an != null) {
-			AutoWireByType au = (AutoWireByType) an;
-			for (Class<?> c : au.value()) {
-				if (c.getName().equalsIgnoreCase(typeName)) {
-					succeed = true;
-				}
-			}
-		}
+        return field;
+    }
 
-		succeed = ctField.getAnnotation(anClz) != null ? true : succeed;
+    protected boolean check(CtField ctField, String typeName, Class<? extends Annotation> anClz)
+            throws ClassNotFoundException, NotFoundException {
+        boolean succeed = false;
+        Annotation an = null;
 
-		return succeed;
-	}
+        an = anClz.getAnnotation(AutoWireByName.class);
+        if (an != null) {
+            AutoWireByName au = (AutoWireByName) an;
+            if (au.value().indexOf(ctField.getName() + ";") >= 0) {
+                succeed = true;
+            }
+        }
 
-	protected boolean check(CtClass ctClass, String typeName, Class<? extends Annotation> anClz)
-			throws ClassNotFoundException, NotFoundException {
-		boolean succeed = false;
-		Annotation an = null;
+        an = anClz.getAnnotation(AutoWireByType.class);
+        if (an != null) {
+            AutoWireByType au = (AutoWireByType) an;
+            for (Class<?> c : au.value()) {
+                if (c.getName().equalsIgnoreCase(typeName)) {
+                    succeed = true;
+                }
+            }
+        }
 
-		an = anClz.getAnnotation(AutoWireByName.class);
-		if (an != null) {
-			AutoWireByName au = (AutoWireByName) an;
-			if (au.value().indexOf(ctClass.getName() + ";") >= 0) {
-				succeed = true;
-			}
-		}
+        succeed = ctField.getAnnotation(anClz) != null ? true : succeed;
 
-		an = anClz.getAnnotation(AutoWireByType.class);
-		if (an != null) {
-			AutoWireByType au = (AutoWireByType) an;
-			for (Class<?> c : au.value()) {
-				if (c.getName().equalsIgnoreCase(typeName)) {
-					succeed = true;
-				}
-			}
-		}
+        return succeed;
+    }
 
-		succeed = ctClass.getAnnotation(anClz) != null ? true : succeed;
+    protected boolean check(CtClass ctClass, String typeName, Class<? extends Annotation> anClz)
+            throws ClassNotFoundException, NotFoundException {
+        boolean succeed = false;
+        Annotation an = null;
 
-		return succeed;
-	}
+        an = anClz.getAnnotation(AutoWireByName.class);
+        if (an != null) {
+            AutoWireByName au = (AutoWireByName) an;
+            if (au.value().indexOf(ctClass.getName() + ";") >= 0) {
+                succeed = true;
+            }
+        }
 
-	protected boolean check(CtField ctField, CtClass ctType, Class<? extends Annotation> anClz)
-			throws ClassNotFoundException, NotFoundException {
-		boolean succeed = false;
-		Annotation an = null;
+        an = anClz.getAnnotation(AutoWireByType.class);
+        if (an != null) {
+            AutoWireByType au = (AutoWireByType) an;
+            for (Class<?> c : au.value()) {
+                if (c.getName().equalsIgnoreCase(typeName)) {
+                    succeed = true;
+                }
+            }
+        }
 
-		an = anClz.getAnnotation(AutoWireByName.class);
-		if (an != null) {
-			AutoWireByName au = (AutoWireByName) an;
-			if (au.value().indexOf(ctField.getName() + ";") >= 0) {
-				succeed = true;
-			}
-		}
+        succeed = ctClass.getAnnotation(anClz) != null ? true : succeed;
 
-		an = anClz.getAnnotation(AutoWireByType.class);
-		if (an != null) {
-			AutoWireByType au = (AutoWireByType) an;
-			for (Class<?> c : au.value()) {
-				if (c.getName().equalsIgnoreCase(ctType.getName())) {
-					succeed = true;
-				}
-			}
-		}
+        return succeed;
+    }
 
-		succeed = ctType.getAnnotation(anClz) != null ? true : succeed;
-		succeed = ctField.getAnnotation(anClz) != null ? true : succeed;
+    protected boolean check(CtField ctField, CtClass ctType, Class<? extends Annotation> anClz)
+            throws ClassNotFoundException, NotFoundException {
+        boolean succeed = false;
+        Annotation an = null;
 
-		return succeed;
-	}
+        an = anClz.getAnnotation(AutoWireByName.class);
+        if (an != null) {
+            AutoWireByName au = (AutoWireByName) an;
+            if (au.value().indexOf(ctField.getName() + ";") >= 0) {
+                succeed = true;
+            }
+        }
 
-	protected ArrayList<String> decorateActualTypeArguments(CtField v) {
-		SignatureAttribute s = (SignatureAttribute) v.getFieldInfo().getAttribute(SignatureAttribute.tag);
-		assert s != null;
+        an = anClz.getAnnotation(AutoWireByType.class);
+        if (an != null) {
+            AutoWireByType au = (AutoWireByType) an;
+            for (Class<?> c : au.value()) {
+                if (c.getName().equalsIgnoreCase(ctType.getName())) {
+                    succeed = true;
+                }
+            }
+        }
 
-		String sig = s.getSignature();
-		ArrayList<String> params = new ArrayList<String>();
+        succeed = ctType.getAnnotation(anClz) != null ? true : succeed;
+        succeed = ctField.getAnnotation(anClz) != null ? true : succeed;
 
-		int pos = 0;
+        return succeed;
+    }
 
-		assert sig.charAt(pos) == 'L';
+    protected ArrayList<String> decorateActualTypeArguments(CtField v) {
+        SignatureAttribute s = (SignatureAttribute) v.getFieldInfo().getAttribute(SignatureAttribute.tag);
+        assert s != null;
 
-		pos++;
+        String sig = s.getSignature();
+        ArrayList<String> params = new ArrayList<String>();
 
-		int start = pos;
-		while (sig.charAt(++pos) != '<');
+        int pos = 0;
 
-		String typename = sig.substring(start, pos).replace('/', '.');
+        assert sig.charAt(pos) == 'L';
 
-		assert typename.equals(noc.lang.List.class.getName());
-		pos++;
+        pos++;
 
-		do {
-			String pam;
-			if (sig.charAt(pos) != 'L') break;
+        int start = pos;
+        while (sig.charAt(++pos) != '<')
+            ;
 
-			pos++;
+        String typename = sig.substring(start, pos).replace('/', '.');
 
-			start = pos;
-			while (sig.charAt(++pos) != ';');
+        assert typename.equals(noc.lang.List.class.getName());
+        pos++;
 
-			pam = sig.substring(start, pos).replace('/', '.');
-			params.add(pam);
-			pos++;
-		} while (sig.charAt(pos) != '>');
+        do {
+            String pam;
+            if (sig.charAt(pos) != 'L')
+                break;
 
-		return params;
-	}
+            pos++;
+
+            start = pos;
+            while (sig.charAt(++pos) != ';')
+                ;
+
+            pam = sig.substring(start, pos).replace('/', '.');
+            params.add(pam);
+            pos++;
+        } while (sig.charAt(pos) != '>');
+
+        return params;
+    }
 }
