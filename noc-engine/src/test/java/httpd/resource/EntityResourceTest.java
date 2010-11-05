b4de5d9e288ce3f4f67caa0ae0ac79490cdb28c2
wangshilian <wangshilian@gmail.com>
2010-11-05 11:07:31 +0800

    减少测试代码loop数

--- noc-engine/src/test/java/httpd/resource/EntityResourceTest.java
+++ noc-engine/src/test/java/httpd/resource/EntityResourceTest.java
@@ -104,8 +104,8 @@ public class EntityResourceTest extends TestCase {
 
         System.out.println("Update START");
 
-        int outloop = 100;
-        int loop = 1000;
+        int outloop = 1;
+        int loop = 1;
         long start;
         long end ;
         long costa;
@@ -141,48 +141,5 @@ public class EntityResourceTest extends TestCase {
         }
     }
 
-    // public void testUpdate() {
-    // int loop = 1000;
-    // Map<String, String> form =
-    // fromString("code=codevalue&code02=code02value&code03=code03value&code04=code04value&code05=code05value&code06=code06value&code07=code07value&code08=code08value&code09=code09value&code10=code10value&code11=code11value&code12=code12value&code13=code13value&code14=code14value&code15=code15value&code16=code16value&code17=code17value&code18=code18value&code19=code19value&code20=code20value&code21=code21value&code22=code22value&code23=code23value&code24=code24value&code25=code25value&code26=code26value&code27=code27value&code28=code28value&code29=code29value&code30=code30value&code31=code31value&code32=code32value&code33=code33value&code34=code34value&code35=code35value&code36=code36value&code37=code37value&code38=code38value&code39=code39value&code40=code40value&code41=code41value&code42=code42value&code43=code43value&code44=code44value&code45=code45value&code46=code46value&code47=code47value&code48=code48value&code49=code49value&code50=code50value&code51=code51value&code52=code52value&code53=code53value&code54=code54value&code55=code55value&code56=code56value&code57=code57value&code58=code58value&code59=code59value&code60=code60value&code61=code61value&code62=code62value&code63=code63value&code64=code64value&code65=code65value&code66=code66value&code67=code67value&code68=code68value&code69=code69value&code70=code70value&code71=code71value&code72=code72value&code73=code73value&code74=code74value&code75=code75value&code76=code76value&code77=code77value&code78=code78value&code79=code79value&code80=code80value&code81=code81value&code82=code82value&code83=code83value&code84=code84value&code85=code85value&code86=code86value&code87=code87value&code88=code88value&code89=code89value&code90=code90value&code91=code91value&code92=code92value&code93=code93value&code94=code94value&code95=code95value&code96=code96value&code97=code97value&code98=code98value&code99=code99value&code100=code100value&Submit=submit");
-    //
-    // form.put("code02", new Date().toString());
-    //
-    // System.out.println("Update START - " + store.list().size());
-    // long start = System.currentTimeMillis();
-    // for (int i = 0; i < loop; i++) {
-    // String key = "codevalue" + i;
-    // form.put("code", key);
-    //
-    // Vo dest = (Vo) store.borrowData(key);
-    // dest = VoHelper.putAll(form, dest, this.type);
-    // store.returnData(dest.getIndentify(), dest);
-    // }
-    // long end = System.currentTimeMillis();
-    // System.out.println("Update FINISH at " + (end - start) + " : " + (end -
-    // start) / loop + " - "
-    // + store.list().size());
-    // }
-    //
-    // public void testAdd() {
-    // int loop = 1000;
-    // Map<String, String> form =
-    // fromString("code=codevalue&code02=code02value&code03=code03value&code04=code04value&code05=code05value&code06=code06value&code07=code07value&code08=code08value&code09=code09value&code10=code10value&code11=code11value&code12=code12value&code13=code13value&code14=code14value&code15=code15value&code16=code16value&code17=code17value&code18=code18value&code19=code19value&code20=code20value&code21=code21value&code22=code22value&code23=code23value&code24=code24value&code25=code25value&code26=code26value&code27=code27value&code28=code28value&code29=code29value&code30=code30value&code31=code31value&code32=code32value&code33=code33value&code34=code34value&code35=code35value&code36=code36value&code37=code37value&code38=code38value&code39=code39value&code40=code40value&code41=code41value&code42=code42value&code43=code43value&code44=code44value&code45=code45value&code46=code46value&code47=code47value&code48=code48value&code49=code49value&code50=code50value&code51=code51value&code52=code52value&code53=code53value&code54=code54value&code55=code55value&code56=code56value&code57=code57value&code58=code58value&code59=code59value&code60=code60value&code61=code61value&code62=code62value&code63=code63value&code64=code64value&code65=code65value&code66=code66value&code67=code67value&code68=code68value&code69=code69value&code70=code70value&code71=code71value&code72=code72value&code73=code73value&code74=code74value&code75=code75value&code76=code76value&code77=code77value&code78=code78value&code79=code79value&code80=code80value&code81=code81value&code82=code82value&code83=code83value&code84=code84value&code85=code85value&code86=code86value&code87=code87value&code88=code88value&code89=code89value&code90=code90value&code91=code91value&code92=code92value&code93=code93value&code94=code94value&code95=code95value&code96=code96value&code97=code97value&code98=code98value&code99=code99value&code100=code100value&Submit=submit");
-    // form.put("code02", new Date().toString());
-    //
-    // System.out.println("Add START - " + store.list().size());
-    // long start = System.currentTimeMillis();
-    // for (int i = 0; i < loop; i++) {
-    // String key = "codevalue" + i + " - " + System.currentTimeMillis();
-    // Vo dest = (Vo) store.borrowData(null);
-    // form.put("code", key);
-    // dest = VoHelper.putAll(form, dest, this.type);
-    // store.returnData(dest.getIndentify(), dest);
-    // }
-    // long end = System.currentTimeMillis();
-    // System.out.println("Add FINISH at " + (end - start) + " : " + (end -
-    // start) / loop + " - "
-    // + store.list().size());
-    // }
 
 }
