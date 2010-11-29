9461505ddfe3035452403b328dd2aaeb8a1d4067
wangshilian <wangshilian@gmail.com>
2010-11-29 15:54:40 +0800

    change to parent/child model

---
+++ conf/clean_eclipse.bat
@@ -0,0 +1,36 @@
+cd ..
+
+
+cd noc-define
+cmd /c mvn eclipse:clean
+
+cd ..
+
+cd noc-frame
+cmd /c mvn eclipse:clean
+
+cd ..
+
+cd noc-engine
+cmd /c mvn eclipse:clean
+
+cd ..
+
+cd test-kao
+cmd /c mvn eclipse:clean
+
+cd ..
+
+cd test-ooj
+cmd /c mvn eclipse:clean
+
+cd ..
+
+cd noc-site
+cmd /c mvn eclipse:clean
+
+cd ..
+
+cd conf
+
+
