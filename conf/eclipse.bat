277700df30311d1ff36cd97cc1b08347435cf972
wangshilian <wangshilian@gmail.com>
2010-11-29 11:01:16 +0800

    change ignore file

---
+++ conf/eclipse.bat
@@ -0,0 +1,34 @@
+cd ..
+cd noc-define
+cmd /c mvn eclipse:clean
+cmd /c mvn eclipse:eclipse
+cd ..
+
+cd noc-frame
+cmd /c mvn eclipse:clean
+cmd /c mvn eclipse:eclipse
+cd ..
+
+cd noc-engine
+cmd /c mvn eclipse:clean
+cmd /c mvn eclipse:eclipse
+cd ..
+
+cd test-kao
+cmd /c mvn eclipse:clean
+cmd /c mvn eclipse:eclipse
+cd ..
+
+cd test-ooj
+cmd /c mvn eclipse:clean
+cmd /c mvn eclipse:eclipse
+cd ..
+
+cd noc-site
+cmd /c mvn eclipse:clean
+cmd /c mvn eclipse:eclipse
+cd ..
+
+cd conf
+
+
